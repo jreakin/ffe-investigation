04f9f0f774f4d882dcda43431725b52bea182ccf
jreakin <johnreakin@gmail.com>
2024-10-20 16:03:27 -0500

    mkdocs change

--- site/dark_mode.js
+++ site/dark_mode.js
@@ -11,8 +11,8 @@ gtag('js', new Date());
 gtag('config', 'G-FF4SS4SRX9');
 
 // Clarity
-(function(c,l,a,r,i,t,y){
-    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-})(window, document, "clarity", "script", "olrkim6xfb");
+function(c,l,a,r,i,t,y){
+c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+}(window, document, "clarity", "script", "olrkim6xfb");
